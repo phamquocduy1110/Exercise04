648ba81e2bd9f62bd3d09396b689d3624209e393
phamquocduy1110 <phamquocduy1110@gmail.com>
2019-09-26 14:54:30 +0700

    Test Odd and Even Number

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace Exercise04
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Word");
+            Console.Write("Please Import A Value:  ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Odd_or_Even_Number(input);
+        }
+        static void Odd_or_Even_Number(int n)
+        {
+            if(Even_Number(n))
+                Console.WriteLine("{0} is even number", n);
+            if(Odd_Number(n))
+                Console.WriteLine("{0} is even number", n);
+        }
+       
+        static bool Even_Number(int n)
+        {
+            return n % 2 == 0;
+        }
+
+        public static bool Odd_Number(int n)
+        {
+            return n % 2 == 1;
         }
     }
 }
